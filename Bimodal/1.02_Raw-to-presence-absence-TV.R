8cebc874d30e936b6c9ef4db44d55e25d365b90e
anitas-giraldo <anita.giraldo@marineecology.io>
2020-07-14 15:57:37 +0800

    commit

--- Bimodal/1.02_Raw-to-presence-absence-TV.R
+++ Bimodal/1.02_Raw-to-presence-absence-TV.R
@@ -9,7 +9,9 @@ library(readr)
 library(stringr)
 library(readr)
 library(devtools)
-#install.packages("remotes")
+library(sp)
+library(raster)
+library(rgdal)#install.packages("remotes")
 #remotes::install_github("UWAMEGFisheries/GlobalArchive")
 #library(GlobalArchive)
 
@@ -118,4 +120,153 @@ head(hab.points.zone)
 
 write.csv(hab.points.zone, paste(tidy.dir, paste(study, "seag-pres-abs.csv", sep='-'), sep='/'))
 
-hab.points <- read.csv(paste(tidy.dir, paste(study, "seag-pres-abs.csv", sep='-'), sep='/'))
+
+## Get data ready for epower ----
+
+hab.points.zone <- read.csv(paste(tidy.dir, paste(study, "seag-pres-abs.csv", sep='-'), sep='/'))
+str(hab.points.zone)
+
+
+df <- hab.points.zone
+
+# Remove NA's -  in the zone column --
+#df <- na.omit(df)
+df <- df %>% drop_na(ZoneName)
+str(df)# 25340 obs
+
+# Remove unwanted columns
+names(df)
+df <- df[, c(4:6,9:12,20)]
+names(df)
+head(df)
+
+
+### split the data by zone name--
+dfz <- split(df, df$ZoneName)
+str(dfz)
+# remove special purpose zone
+dfn <-  dfz[-3]
+str(dfn)
+
+# new listo into data frame
+dfn <- do.call(rbind.data.frame, dfn)
+dfn
+str(dfn)
+names(dfn)
+head(dfn)
+summary(dfn)
+
+dfn <- droplevels(dfn)
+
+levels(dfn$ZoneName)
+
+## Make T2 part of HPZ and  Make T6 and T7 part of another zone
+trans <- dfn$Transect
+levels(trans)
+dfn$zone.sim <- factor(trans, labels = c("HPZ", "HPZ", "MUZ", "MUZ", "MUZ2", "MUZ2"))
+summary(dfn)
+str(dfn)
+
+levels(dfn$zone.sim)
+
+#### T1 ####
+
+#subsample dat to n=1900(76 images/25 points per transect that is the max in on of the transects) to improve speed
+
+dfn1 <-as.data.frame(dfn %>% group_by(Transect) %>% sample_n(size = 1900))
+str(dfn1)
+dfn1 <- droplevels(dfn1)
+str(dfn1) # 11250 obs
+
+## calculate presences and no. scored ----
+
+sg.pres1 <- aggregate(Seagrass ~ Transect + ZoneName, data = dfn1, sum)
+sg.pres1
+
+no.scored1 <-  aggregate(Seagrass ~ Transect + ZoneName, data = dfn1, length)           
+no.scored1
+names(no.scored1) <- c("Transect", "ZoneName", "no.scored")
+
+df1 <- cbind(sg.pres1, no.scored1[,3])
+df1
+names(df1) <- c("Transect",        "ZoneName",       "Seagrass",    "no.scored")
+df1$Time <- "T1"
+df1
+
+#### T2 ####
+
+#subsample dat to n=1900(76 images/25 points per transect that is the max in on of the transects) to improve speed
+
+dfn2 <-as.data.frame(dfn %>% group_by(Transect) %>% sample_n(size = 1900))
+str(dfn2)
+dfn2 <- droplevels(dfn2)
+str(dfn2) # 11250 obs
+
+## calculate presences and no. scored ----
+
+sg.pres2 <- aggregate(Seagrass ~ Transect + ZoneName, data = dfn2, sum)
+sg.pres2
+
+no.scored2 <-  aggregate(Seagrass ~ Transect + ZoneName, data = dfn2, length)           
+no.scored2
+names(no.scored2) <- c("Transect", "ZoneName", "no.scored")
+
+df2 <- cbind(sg.pres2, no.scored2[,3])
+df2
+names(df2) <- c("Transect",        "ZoneName",       "Seagrass",    "no.scored")
+df2$Time <- "T2"
+df2
+
+#### T3 ####
+
+#subsample dat to n=1900(76 images/25 points per transect that is the max in on of the transects) to improve speed
+
+dfn3 <-as.data.frame(dfn %>% group_by(Transect) %>% sample_n(size = 1900))
+str(dfn3)
+dfn3 <- droplevels(dfn3)
+str(dfn3) # 11250 obs
+
+## calculate presences and no. scored ----
+
+sg.pres3 <- aggregate(Seagrass ~ Transect + ZoneName, data = dfn3, sum)
+sg.pres3
+
+no.scored3 <-  aggregate(Seagrass ~ Transect + ZoneName, data = dfn3, length)           
+no.scored3
+names(no.scored3) <- c("Transect", "ZoneName", "no.scored")
+
+df3 <- cbind(sg.pres3, no.scored3[,3])
+df3
+names(df3) <- c("Transect",        "ZoneName",       "Seagrass",    "no.scored")
+df3$Time <- "T3"
+df3
+
+## joint these together --
+
+dfall <- rbind(df1, df2, df3)
+dfall
+
+
+# Make Period Column
+dfall$Period <- "Before"
+names(dfall)
+
+## Make T2 part of HPZ and  Make T6 and T7 part of another zone
+trans <- dfall$Transect
+levels(trans)
+dfall$zone.sim <- factor(trans, labels = c("HPZ", "HPZ", "MUZ", "MUZ", "MUZ2", "MUZ2"))
+summary(dfall)
+
+levels(dfall$zone.sim)
+
+# Make control impact column
+levels(dfall$zone.sim)
+dfall$CvI <- ifelse(dfall$zone.sim=="HPZ", "Impact", "Control")
+head(dfall)
+
+
+#### Save data for epower ----
+
+write.csv(dfall, paste(tidy.dir, paste(study, "seag-epower.csv", sep='-'), sep='/'))
+
+
