1ff2899522d60dd51c653409a60a63705397b44c
Kingsley Griffin <kingsleyg88@gmail.com>
2021-08-09 15:48:49 +0800

    running power analysis for boss

--- 3.PowerAnalysis_Boss.R
+++ 3.PowerAnalysis_Boss.R
@@ -21,7 +21,6 @@ rm(list=ls())
 # Load data
 
 dwndat <- readRDS("data/tidy/2103_Geographe_BOSS_downwardshabitat.rds")
-summary(dwndat)
 
 # Remove the Special purpose zone
 dwndat <- dwndat[dwndat$ZoneName != "Special Purpose Zone (Mining Exclusion)", ]
@@ -30,11 +29,11 @@ dwndat <- dwndat[dwndat$ZoneName != "Special Purpose Zone (Mining Exclusion)", ]
 dwndat$Period <- "Before"                                                       # Make Period Column
 dwndat$CI     <- ifelse(dwndat$ZoneName == "Habitat Protection Zone", 
                         "Impact", "Control")                                    # Make control impact column
-dwndat$Time   <- "T1"
-head(dwndat)
-
-dwndat <- rbind(dwndat, dwndat)
-dwndat$Time[(((nrow(dwndat)/2)+1)):nrow(dwndat)] <- c("T2")                     # dummy T2
+# dwndat$CI[dwndat$ZoneName == "Multiple Use Zone"] <- c("Control2")
+dwndat <- rbind(dwndat, dwndat, dwndat)
+dwndat$Time <- c(rep("T1", nrow(dwndat)/3), 
+                 rep("T2", nrow(dwndat)/3), 
+                 rep("T3", nrow(dwndat)/3))                                     # dummy times
 
 str(dwndat)
 dwndat$Period <- as.factor(dwndat$Period)
@@ -69,10 +68,10 @@ dataComponents<-supplyData(
     Impact="Impact"),
   scenario.data=list(
     Number.of.iterations=5,
-    filename="Tv-test1",
+    filename="Boss-test1",
     Number.of.Impact.Locations=1,
-    Number.of.Control.Locations=1,
-    Number.of.sublocations.within.Location="2;3",
+    Number.of.Control.Locations=2,
+    Number.of.sublocations.within.Location=NA,
     Number.of.sample.times.Before=2,
     Number.of.sample.times.After="1;2;3",
     Number.of.subtimes.within.Time=NA,
@@ -81,7 +80,7 @@ dataComponents<-supplyData(
   effect.info=list(
     Multiplicative=1,
     Fixed.change=0,
-    Effect.values="0;-0.2;-0.4;-0.6,-0.8"), ncores = 2)
+    Effect.values="0;-0.2;-0.4;-0.6,-0.8"), ncores = 8)
 
 
 #require(INLA,quietly=TRUE)
